783ca74640fad192673e647201212c3399a4b74d
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-11-01 18:58:30 -0400

    captured form input and appended to task list in index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,27 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do")
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "New task";
+    //get task name from form input
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskNameDisplay = taskNameInput.charAt(0).toUpperCase() + taskNameInput.slice(1);
+    
+    //get task language from form drop down
+    var taskLangInput = document.querySelector("select[name='task-language']").value;
+
+    //create div to hold task language
+    var taskLangEl = document.createElement("div");
+    taskLangEl.className = "task-info";
+    //add HTML to div
+    taskLangEl.innerHTML = "<h3 class='task-name'>" + taskNameDisplay + "</h3><span class='task-type'>" + taskLangInput + "</span>";
+    //append div to li
+    listItemEl.appendChild(taskLangEl);
+    
+    //add li to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
