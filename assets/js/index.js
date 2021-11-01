3aa365143690fe3fa0367ee6c34489dd77dff3dd
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-11-01 19:12:37 -0400

    javascript refactor

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,41 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do")
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
     //get task name from form input
     var taskNameInput = document.querySelector("input[name='task-name']").value;
+    //capitalize first letter
     var taskNameDisplay = taskNameInput.charAt(0).toUpperCase() + taskNameInput.slice(1);
     
     //get task language from form drop down
     var taskLangInput = document.querySelector("select[name='task-language']").value;
 
-    //create div to hold task language
-    var taskLangEl = document.createElement("div");
-    taskLangEl.className = "task-info";
-    //add HTML to div
-    taskLangEl.innerHTML = "<h3 class='task-name'>" + taskNameDisplay + "</h3><span class='task-type'>" + taskLangInput + "</span>";
-    //append div to li
-    listItemEl.appendChild(taskLangEl);
-    
-    //add li to list
-    tasksToDoEl.appendChild(listItemEl);
+    //package form data into object
+    var taskDataObj = {
+        name: taskNameDisplay,
+        type: taskLangInput
+    }
+
+    //call function to create task and pass in task object data
+    createTaskEl(taskDataObj);
+
 }
 
-formEl.addEventListener("submit", createTaskHandler);
+var createTaskEl = function(taskDataObj) {
+    //create li
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+     //create div to hold task language
+     var taskLangEl = document.createElement("div");
+     taskLangEl.className = "task-info";
+     //add HTML to div
+     taskLangEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+     //append div to li
+     listItemEl.appendChild(taskLangEl);
+     
+     //add li to list
+     tasksToDoEl.appendChild(listItemEl);
+}
+
+formEl.addEventListener("submit", taskFormHandler);
